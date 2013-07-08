8cbd0228ed21c0d0c5b607303c554acfbeaf11db
Robin Martens <robin_martens@brown.edu>
2013-07-08 08:20:08 -0400

    work on 'juliaset.cu' (incomplete)

---
+++ juliaset.cu
@@ -0,0 +1,53 @@
+#include "/home/robin/src/cudapractice/support/common/book.h"
+#include "/home/robin/src/cudapractice/support/common/cpu_bitmap.h"
+
+#define DIM 1000
+
+struct cuComplex {
+	float r;
+	float i;
+	cuComple( float a, float b ) : r(a), i(b) {}
+	__device__ float magnitude2( void ) {
+		return r * r + i * i;
+	}
+	__device__ cuComplex operator*(const cuComplex& a) {
+		return cuComplex(r*a.r - i*a.i, i*a.r + r*a.i);
+	}
+	__device__ cuComplex operator+(const cuComplex& a) {
+		return cuComplex(r+a.r, i+a.i);
+	}
+};
+
+__device__ julia( int x, int y ) {
+	const float scale = 1.5;
+	float jx = scale * (float) (DIM/2 - x)/(DIM/2);
+	float jy = scale * (float) (DIM/2 - y)/(DIM/2);
+
+	cuComplex c(-0.8, 0.156);
+	cuComplex a(jx, jy);
+
+	int i = 0;
+	for (i=0; i<200; i++) {
+		a = a * a + c;
+		if (a.magnitude2() > 1000) {
+			return 0;
+		}
+	}
+
+	return 1;
+}
+
+__global__ void kernel( unsigned char *ptr) {
+	// map from blockIdx to pixel position
+	int x = blockIdx.x;
+	int y = blockIdx.y;
+	int offset = x + y * gridDim.x
+
+	// now calculate that value at that position
+	int juliaValue = julia( x, y );
+	ptr[offset*4 + 0] = 255 * juliaValue;
+	ptr[offset*4 + 1] = 0;
+	ptr[offset*4 + 2] = 0;
+	ptr[offset*4 + 3] = 255;
+}
+
