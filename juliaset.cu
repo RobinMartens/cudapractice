1dfac9906955a7e5568a23dc38926d9dbbc716a6
Robin Martens <robin_martens@brown.edu>
2013-07-08 14:20:19 -0400

    add 'juliaset.cu'

--- juliaset.cu
+++ juliaset.cu
@@ -51,3 +51,19 @@ __global__ void kernel( unsigned char *ptr) {
 	ptr[offset*4 + 3] = 255;
 }
 
+
+int main( void ) {
+    CPUBitmap bitmap( DIM, DIM );
+    unsigned char *dev_bitmap;
+
+	HANDLE_ERROR( cudaMalloc( (void**)&dev_bitmap, bitmap.image_size() ) );
+	
+	dim3 grid(DIM,DIM);
+	kernel<<<grid,1>>>( dev_bitmap );
+	
+	HANDLE_ERROR( cudaMemcpy( bitmap.get_ptr(), dev_bitmap, bitmap.image_size(), cudaMemcpyDeviceToHost ) );
+
+	bitmap.display_and_exit();
+
+	HANDLE_ERROR( cudaFree( dev_bitmap ) );
+}
